032b7990e63267163ce2cbc403780e07e4dd32ad
domberman <48616609+domberman@users.noreply.github.com>
2019-05-22 14:49:32 +0300

    memory manager

---
+++ MemoryManager.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class MemoryManager {
+
+    public char[][] getBoard(String level){
+        char[][] board;
+        File file = new File(System.getProperty("user.dir")+"/" + level + ".txt");
+        Scanner sc;
+        try {
+             sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            return  null;
+        }
+        int w = 0;  //width
+        int h = 0;  //height
+        if (sc.hasNextLine()){
+            w = sc.nextLine().length();
+            h++;
+        }
+        else{
+            return null;
+        }
+        while (sc.hasNextLine()){
+            sc.nextLine();
+            h++;
+        }
+        board = new char[h][w];
+        try {
+            sc = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            return  null;
+        }
+        for (int i = 0; i < h; i++) {
+            String s = "";
+            if (sc.hasNextLine()) {
+                s = sc.nextLine();
+            }
+            for (int j = 0; j < w; j++) {
+                board[i][j] = s.charAt(i);
+            }
+        }
+        return board;
+    }
+}
